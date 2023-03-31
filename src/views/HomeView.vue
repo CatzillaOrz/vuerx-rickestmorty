4701accc305a3c6b8ceb5ad1b480f3521c73146a
catzilla <catzillaorz@outlook.com>
2023-03-31 23:24:54 +0800

    feat: domStreams replacement useage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,18 +2,19 @@
   <div class="home">
     <img alt="Vue logo" v-if="image$" :src="image$" />
     <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
+    <button v-stream:click="plus$">+</button>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-import { Character, getCharacter } from "rickmortyapi";
-import { from, Observable } from "rxjs";
+//import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { getCharacter } from "rickmortyapi";
+import { from, Observable, Subject } from "rxjs";
 import { map, share, exhaustMap, delay, tap } from "rxjs/operators";
 
 export const source$ = from(getCharacter(1)).pipe(share(), delay(3000));
-@Component({
+@Component<HomeView>({
   components: {
     // HelloWorld,
   },
@@ -22,10 +23,13 @@ export const source$ = from(getCharacter(1)).pipe(share(), delay(3000));
       map((res) => res.data.image),
       tap((e) => console.log(e))
     );
+    console.log(this.plus$);
     return { image$ };
   },
 })
 export default class HomeView extends Vue {
+  test = "hello world";
+  plus$ = new Subject();
   mounted() {
     // this.subscriptions();
   }
